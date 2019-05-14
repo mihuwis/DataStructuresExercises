35fb5696ea0f494ca4aadbae5563422a128c2701
Michal Wisniewski <mihuwis@gmail.com>
2019-05-14 12:48:40 +0200

    First commit with projct. Maven quickstart architecture

---
+++ src/main/java/com/mihu/App.java
@@ -0,0 +1,13 @@
+package com.mihu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
