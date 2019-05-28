0b815e9934a68c070d7c37f812f97e852e61e7f1
Michal Wisniewski <mihuwis@gmail.com>
2019-05-28 11:19:08 +0200

    Add Factorial algorythm

--- src/main/java/com/mihu/Algorythms/Factorial.java
+++ src/main/java/com/mihu/Algorythms/Factorial.java
@@ -1,4 +1,19 @@
 package com.mihu.Algorythms;
 
+import java.util.stream.LongStream;
+
 public class Factorial {
+
+    public long factorial(long number ) {
+        if (number == 0) {
+            return 1;
+        }
+        return number * factorial(number - 1);
+    }
+
+    public long factorialStream(long number){
+
+        return LongStream.rangeClosed(1, number)
+                .reduce(1, (long a, long b) -> a * b);
+    }
 }
--- src/test/java/com/mihu/Algorythms/FactorialTest.java
+++ src/test/java/com/mihu/Algorythms/FactorialTest.java
@@ -1,7 +1,54 @@
 package com.mihu.Algorythms;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FactorialTest {
+
+    private Factorial factorial;
+
+    @BeforeEach
+    private void createInstance(){
+        this.factorial = new Factorial();
+    }
+
+    @Test
+    public void test5is120(){
+        long expected = 120;
+        assertEquals(expected, factorial.factorial(5));
+    }
+
+    @Test
+    public void test0is1(){
+        long expected = 1;
+        assertEquals(expected, factorial.factorial(0));
+    }
+
+    @Test
+    public void test1is1(){
+        long expected = 1;
+        assertEquals(expected, factorial.factorial(1));
+    }
+
+    @Test
+    public void test2is2(){
+        long expected = 2;
+        assertEquals(expected, factorial.factorial(2));
+    }
+
+    @Test
+    public void test3is6(){
+        long expected = 6;
+        assertEquals(expected, factorial.factorial(3));
+    }
+
+    @Test
+    public void test3is6stream() {
+        long expected = 6;
+        assertEquals(expected, factorial.factorialStream(3));
+    }
+
 
 }
